25ee0c1c4b617e949c2203f52d16915348b274a1
yingchuanfu <245160096@qq.com>
2018-12-16 23:25:05 +0800

    study activiti6 repository init

---
+++ activiti6/src/main/java/com/imooc/activiti/Activiti6Application.java
@@ -0,0 +1,14 @@
+package com.imooc.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activiti6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activiti6Application.class, args);
+	}
+
+}
+

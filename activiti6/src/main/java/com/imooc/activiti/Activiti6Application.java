8e599acc6e2b090f45a8634bb0fa2cf0d2436a15
yingchuanfu <245160096@qq.com>
2018-12-18 20:29:54 +0800

    activiti6.0

--- activiti6/src/main/java/com/imooc/activiti/Activiti6Application.java
+++ activiti6/src/main/java/com/imooc/activiti/Activiti6Application.java
@@ -2,10 +2,16 @@ package com.imooc.activiti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Activiti6Application {
+public class Activiti6Application extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Activiti6Application.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Activiti6Application.class, args);
 	}
